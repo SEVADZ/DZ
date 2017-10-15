c05380d1ad83d44acde4c8321182cb670528b63d
SEVADZ <Sattarov.a.2017@fml31.ru>
2017-10-15 19:03:12 +0200

    !

--- Pack2/p2_3.py
+++ Pack2/p2_3.py
@@ -5,7 +5,18 @@ def fact(x1):
         return 1
     else:
         return x1*fact(x1-1)
-
+def sos(q):
+        return round(fact(2 * q) / (fact(q) * fact(q)))
+def ots(n, m):
+        o = rows // 2
+        k = str(sos(o))
+        
+        k = k + " "
+        j = len(k) - len(str(cxy(n,m)))
+        if (m != 0):
+            return str(cxy(n,m)) + (j) * " "
+        else:
+            return (" " * (((len(k) + 1 )// 2) * ((rows - 1) - n)) + str(cxy(n,m)) + (j) * " ")
 def cxy(x, y):
     return (round(fact(x) / (fact(y) * fact(x - y))))
 rows = int(input())
@@ -13,5 +24,6 @@ for i in range(rows):
     s = 1 + i
     st = ""
     for j in range(s):
-        st = st + str(cxy(s - 1 ,j)) + " "
+        skk = str(ots(s - 1, j))
+        st = st + skk
     print(st)
