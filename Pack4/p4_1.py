4cee28b163f88795f1ec61897538f3d3f940af40
SEVADZ <Sattarov.a.2017@fml31.ru>
2017-11-10 21:24:13 +0200

    pervaya zadacha

--- Pack4/p4_1.py
+++ Pack4/p4_1.py
@@ -8,13 +8,13 @@ class analiz(list):
 		l1 = str(self.cmax[1] // 10000000) + "." + str((self.cmax[1] // 1000) % 100) + "." + str((self.cmax[1] // 100000) % 100) + "." + str(self.cmax[1] % 1000)
 		l2 = str(self.cmax[1] // 10000000) + "." + str((self.cmax[1] // 1000) % 100) + "." + str((self.cmax[1] // 100000) % 100 + 1) + "." + str(self.cmax[1] % 1000)
 		print(l1, "to", l2)
-	def ob(self, x):d
-		self.k.add(x.ex)
+	def ob(self, x):
+		#self.k.add(x.ex)
 		if(x.ex in self.k):
 			self.append(x)
-			self.cc += x.kol
+			self.cc += 1#x.kol
 			while (self[0].time + 1000 <= x.time):
-				self.cc -= self[0].kol
+				self.cc -= 1 #self[0].kol
 				del self[0]
 			if (self.cc > self.cmax[0]):
 				self.cmax[0] = int(self.cc)
@@ -28,6 +28,7 @@ class sdelka():
 		self.cost = round(float(l[2]) * 100)
 		self.ex = l[3][0]
 		self.kol = int(l[2])
+print("ВВЕДИТЕ ВСЕ РАССМАТРИВАЕМЫЕ БИРЖИ ОДНОЙ СТРОКОЙ БЕЗ ПРОБЕЛОВ")
 st = input()
 with open("TRD2.csv") as f:
 	l = analiz(st)
--- Pack4/p4_2.py
+++ Pack4/p4_2.py
@@ -10,16 +10,15 @@ def xx(st):
             return "0"
     else:
         return st.split("x")[0]
-print("Введите полином одной переменной x в порядке убывания степеней (без знака умножения, без пробелов,  ^ - знак степени)")
-st = input()
-i = 1
-l = []
-l1 = []
-l.append(st[0])
-j = 0
-ss = set("+-")
-while (len(st) > i):
-    if (st[i] in ss):
+def proiz(st):
+    i = 1
+    l = []
+    l1 = []
+    l.append(st[0])
+    j = 0
+    ss = set("+-")
+    while (len(st) > i):
+     if (st[i] in ss):
         j += 1
         l1.append(st[i])
         l.append(st[i+1])
@@ -27,16 +26,18 @@ while (len(st) > i):
     else:
         l[j] += st[i]
         i += 1
-k = ""
-for i in range(len(l1)):
-    k += l[i] + l1[i]
-k += l[len(l1)]
-rez = ""
+    k = ""
+    for i in range(len(l1)):
+        k += l[i] + l1[i]
+    k += l[len(l1)]
+    rez = ""
 
-for i in range(len(l) - 1):
-    rez += xx(l[i])
-    if (i == len(l) - 2):
-        pass
-    else:
-        rez += l1[i]
-print(rez)
+    for i in range(len(l) - 1):
+        rez += xx(l[i])
+        if (i == len(l) - 2):
+         pass
+        else:
+         rez += l1[i]
+    return rez
+    print("Введите полином одной переменной x в порядке убывания степеней (без знака умножения, без пробелов,  ^ - знак степени)")
+    print(proiz(input()))
